5d7dbebf7fe27232ea343d045e627fbfc58d427e
seso-kdrs <154320642+seso-kdrs@users.noreply.github.com>
2024-04-22 13:09:18 +0000

    Deploying to gh-pages from @ KDRS-SA/kdrs-search-and-view-docs@c2cb2d6faebebac4d59dec5ddd860afdf2254f7b 🚀

--- develop/1.5/assets/js/just-the-docs.js
+++ develop/1.5/assets/js/just-the-docs.js
@@ -553,4 +553,10 @@ jtd.onReady(function(){
 
 })(window.jtd = window.jtd || {});
 
-
+/* Script below makes XML tags above links to their doc pages. Done in javascript because it is a big job to override the 'highlight' functionality */
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('pre code.language-xml').forEach(block => {
+        block.innerHTML = block.innerHTML.replace(/&lt;\/?(\w+)([^&gt;]*?)&gt;/g, (match, p1, p2) =>
+            `<a href="/kdrs-search-and-view-docs/develop/1.5xml/${p1}" class="xml-tag-link">${match}</a>`);
+    });
+});
